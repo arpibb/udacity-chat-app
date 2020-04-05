d71694215fae56e5a0fd7e410782bf9243157a3e
Arpad Bencze <bencze.arpad.21@gmail.com>
2020-04-05 18:37:59 +0200

    chatwindow component ready

--- src/ChatWindow.js
+++ src/ChatWindow.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import MessageList from './MessageList'
 import InputForm from './InputForm'
+import PropTypes from 'prop-types';
 
 const ChatWindow = (props) => {
-  const { username, messages } = props
+  const { username, messages, addMessage } = props
   return(
       <div className="chat-window">
         <h2>Super Awesome Chat</h2>
         <div className="name sender">{username}</div>
 		<MessageList 
-          username = {username}
-          messages = {messages}
+        username = {username}
+        messages = {messages}
       	/>
 		<InputForm 
 			username = {username}
+			addMessage = {addMessage}
 		/>
 		
       </div>
      )
+}
+ChatWindow.propTypes = {
+	addMessage: PropTypes.func.isRequired,
+  	username: PropTypes.string.isRequired,
+  	messages: PropTypes.array.isRequired
 }
 
 export default ChatWindow
