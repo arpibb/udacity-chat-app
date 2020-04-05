2b13018aa525e8e126a2886a86a9eaafd8db0f60
Arpad Bencze <bencze.arpad.21@gmail.com>
2020-04-05 18:40:26 +0200

    organizing app.js

--- src/App.js
+++ src/App.js
@@ -2,37 +2,22 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ChatWindow from './ChatWindow'
-/*
-This exercise will help you practice many of your newly aquired React skills.
-
-The instructions are included in the `instructions.md` file.
-*/
 
 const users = [{ username: 'Amy' }, { username: 'John' }];
 
-const messages = [
-  { username: 'Amy', text: 'Hi, Jon!' },
-  { username: 'Amy', text: 'How are you?' },
-  { username: 'John', text: 'Hi, Amy! Good, you?' },
-];
-
 class App extends Component {
   state = {
-    users: [{ username: 'Amy' },
-            { username: 'John' }],
-    messages: [{ username: 'Amy', text: 'Hi, Jon!' },
-              { username: 'Amy', text: 'How are you?' },
-              { username: 'John', text: 'Hi, Amy! Good, you?' }],
+    messages: [],
   }
-  /*
-  If the user did not type anything, he/she should not be
-  allowed to submit.
-  */
-	
-	
 
+	addMessage = (username,text) => {
+    	this.setState((prevState)=>({
+        	messages: [...prevState.messages,{username:username, text: text}]
+      }))
+    }
+	
   render() {
-    const {users, messages } = this.state;
+    const { messages } = this.state;
 
     return (
       <div className="App">
@@ -41,71 +26,15 @@ class App extends Component {
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
         <div className="container">
-    	{users.map((user) => {
-          return(
-			<ChatWindow 
-             	key = {user.username}
-				username = {user.username}
-				messages = {messages}
-			/>)})}
-    	
-          <div className="chat-window">
-            <h2>Super Awesome Chat</h2>
-            <div className="name sender">{users[0].username}</div>
-
-            <ul className="message-list">
-              {messages.map((message, index) => (
-                <li
-                  key={index}
-                  className={
-                    message.username === users[0].username ? 'message sender' : 'message recipient'
-                  }
-                >
-                  <p>{`${message.username}: ${message.text}`}</p>
-                </li>
-              ))}
-            </ul>
-
-            <div>
-              <form className="input-group">
-                <input type="text" className="form-control" placeholder="Enter your message..." />
-                <div className="input-group-append">
-                  <button className="btn submit-button" disabled={this.isDisabled()}>
-                    SEND
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
-
-          <div className="chat-window">
-            <h2>Super Awesome Chat</h2>
-            <div className="name sender">{users[1].username}</div>
-            <ul className="message-list">
-              {messages.map((message, index) => (
-                <li
-                  key={index}
-                  className={
-                    message.username === users[1].username ? 'message sender' : 'message recipient'
-                  }
-                >
-                  <p>{`${message.username}: ${message.text}`}</p>
-                </li>
-              ))}
-            </ul>
-
-            <div>
-              <form className="input-group">
-                <input type="text" className="form-control" placeholder="Enter your message..." />
-                <div className="input-group-append">
-                  <button className="btn submit-button" disabled={this.isDisabled()}>
-                    SEND
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
+    	    {users.map((user) => {
+            return(
+              <ChatWindow 
+                key = {user.username}
+                username = {user.username}
+                messages = {messages}
+                addMessage = {this.addMessage}
+			    />)})}
+		    </div>
       </div>
     );
   }
